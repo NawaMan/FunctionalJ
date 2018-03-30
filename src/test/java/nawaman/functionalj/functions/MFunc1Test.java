fe89da24c6eb9bc3a69e5f503d6023215c05e6bb
Nawa Manusitthipol <nawa@nawaman.net>
2018-03-30 11:06:39 -0600

    MFunc1

--- src/test/java/nawaman/functionalj/functions/MFunc1Test.java
+++ src/test/java/nawaman/functionalj/functions/MFunc1Test.java
@@ -14,7 +14,8 @@ public class MFunc1Test {
     public void test() {
         val strLength = MFunc1.of((String  s) -> MayBe.of(s.length()));
         val time2     = MFunc1.of((Integer i) -> MayBe.of(i * 2));
-        val strLengthTimes2 = strLength.chain(time2);
-        assertThat(strLengthTimes2.apply("Hello").toString()).isEqualTo("Just(10)");
+        val plus1     = MFunc1.of((Integer i) -> MayBe.of(i + 1));
+        val strLengthTimes2Plus1 = strLength.chain(time2).chain(plus1);
+        assertThat(strLengthTimes2Plus1.apply("Hello").toString()).isEqualTo("Just(11)");
     }
 }
