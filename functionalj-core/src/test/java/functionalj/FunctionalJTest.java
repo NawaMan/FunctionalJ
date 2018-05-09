963ddd15e68a65bec9d22be724a73f44df5fecef
Nawa Manusitthipol <nawa@nawaman.net>
2018-05-09 00:18:02 -0600

    Remove the old Lens.

--- functionalj-core/src/test/java/functionalj/FunctionalJTest.java
+++ functionalj-core/src/test/java/functionalj/FunctionalJTest.java
@@ -6,44 +6,23 @@ import static functionalj.FunctionalJ.it;
 import static functionalj.FunctionalJ.only;
 import static functionalj.FunctionalJ.withIndex;
 import static functionalj.functions.Absent.__;
-import static functionalj.functions.Absent.absent;
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.singletonMap;
-
-import functionalj.FunctionalJ.WithIndex;
-import functionalj.compose.ArrayLens;
-import functionalj.compose.Lens;
-import functionalj.compose.LensSpec;
-import functionalj.compose.ListLens;
-import functionalj.functions.Absent;
 import functionalj.functions.Func3;
-import lombok.ToString;
 import lombok.Value;
 import lombok.val;
 
@@ -145,155 +124,6 @@ public class FunctionalJTest {
                 + "]",
                 logs.toString());
     }
-    
-    @ToString
-    public static class Person {
-        private final String name;
-        private final City city;
-        private final int age;
-        private final List<String> words;
-        private final Map<String, String> phones;
-        
-        public Person(String name, City city, int age, List<String> words, Map<String, String> phones) {
-            this.name = name;
-            this.city = city;
-            this.age = age;
-            this.words = (words != null) ? words : emptyList();
-            this.phones = (phones != null) ? phones : emptyMap();
-        }
-        
-        public String name()        { return name; }
-        public City city()          { return city; }
-        public int age()            { return age;  }
-        public List<String> words() { return words; }
-        
-        public Person withName(String name)                 { return new Person(name, city, age, words, phones); }
-        public Person withCity(City city)                   { return new Person(name, city, age, words, phones); }
-        public Person withAge(int age)                      { return new Person(name, city, age, words, phones); }
-        public Person withWords(List<String> words)         { return new Person(name, city, age, words, phones); }
-        public Person withPhone(Map<String, String> phones) { return new Person(name, city, age, words, phones); }
-        
-        public static Lens<Person, String> _name = Lens.of(Person::withName, Person::name);
-    }
-    
-    @Test
-    public void testLense() {
-        val personName = Lens.of(Person::withName, Person::name);
-        val personCity = Lens.of(Person::withCity, Person::city);
-        val cityName   = Lens.of(City::withName,   City::getName);
-        val personCityName = personCity.compose(cityName);
-        
-        val person1 = new Person("Nawa", new City("R", "Regina"), 18, asList("Yo"), singletonMap("home", "555-555-5555"));
-        
-        assertEquals("Regina", personCityName.read().apply(person1));
-        
-        assertEquals("FunctionalJTest.Person("
-                + "name=Nawa, "
-                + "city=FunctionalJTest.City("
-                +   "id=R, "
-                +   "name=Regina), "
-                + "age=18, "
-                + "words=[Yo], "
-                + "phones={home=555-555-5555})",
-                person1.toString());
-        
-        assertEquals("FunctionalJTest.Person("
-                + "name=NawaMan, "
-                + "city=FunctionalJTest.City("
-                +   "id=R, "
-                +   "name=Regina), "
-                + "age=18, "
-                + "words=[Yo], "
-                + "phones={home=555-555-5555})",
-                personName.change().apply(person1, "NawaMan").toString());
-        
-        assertEquals("FunctionalJTest.Person("
-                + "name=Nawa, "
-                + "city=FunctionalJTest.City("
-                +   "id=R, "
-                +   "name=Bangkok), "
-                + "age=18, "
-                + "words=[Yo], "
-                + "phones={home=555-555-5555})",
-                personCityName.change().apply(person1, "Bangkok").toString());
-        assertEquals("FunctionalJTest.Person("
-                + "name=Nawa, "
-                + "city=FunctionalJTest.City("
-                +   "id=R, "
-                +   "name=City of Regina), "
-                + "age=18, "
-                + "words=[Yo], "
-                + "phones={home=555-555-5555})",
-                personCityName.map(n->"City of " + n).andThen(Object::toString).apply(person1));
-        
-        String[] s = new String[] { "One", "Two", "Three" };
-        assertEquals("[One, 2, Three]", ArrayLens.at(1).change().andThen(Arrays::toString).apply(s, "2"));
-        
-        List<String> l = asList("One", "Two", "Three");
-        assertEquals("[One, 2, Three]", ListLens.<String>at(1).change().andThen(List::toString).apply(l, "2"));
-        
-        val personWords = Lens.of(Person::withWords, Person::words);
-        val firstWord   = personWords.compose(ListLens.at(0));
-        assertEquals("FunctionalJTest.Person("
-                + "name=Nawa, "
-                + "city=FunctionalJTest.City("
-                +   "id=R, "
-                +   "name=Regina), "
-                + "age=18, "
-                + "words=[Yee], "
-                + "phones={home=555-555-5555})",
-                firstWord.change().andThen(Object::toString).apply(person1, "Yee"));
-        
-        @SuppressWarnings({ "unchecked", "rawtypes" })
-        val map = (Map<String, Object>)(Map)Collections.singletonMap("key", "value");
-        
-        assertEquals("{key=value}",
-                map
-                .toString());
-        
-        val mapKey = Lens.mapAt("key");
-        assertEquals("{key=newValue}",
-                mapKey
-                .change()
-                .apply(map, "newValue")
-                .toString());
-        
-        val mapNewKey = Lens.mapAt("newKey");
-        assertEquals("{newKey=newValue, key=value}",
-                mapNewKey
-                .change()
-                .apply(map, "newValue")
-                .toString());
-    }
-    
-//    @Test
-//    public void testStringLense() {
-//        val personName = new Lens.StringLens<>(Person::withName, Person::name);
-//
-//        val person1 = new Person("Nawa", new City("R", "Regina"), 18, asList("Yo"), singletonMap("home", "555-555-5555"));
-//        
-//        assertEquals("Nawa", personName.read().apply(person1));
-//        
-//        assertEquals("FunctionalJTest.Person("
-//                + "name=Nawa, "
-//                + "city=FunctionalJTest.City("
-//                +   "id=R, "
-//                +   "name=Regina), "
-//                + "age=18, "
-//                + "words=[Yo], "
-//                + "phones={home=555-555-5555})",
-//                person1.toString());
-//        
-//        assertEquals("FunctionalJTest.Person("
-//                + "name=NawaMan, "
-//                + "city=FunctionalJTest.City("
-//                +   "id=R, "
-//                +   "name=Regina), "
-//                + "age=18, "
-//                + "words=[Yo], "
-//                + "phones={home=555-555-5555})",
-//                personName.change().apply(person1, "NawaMan").toString());
-//    }
     
     // Array, List, Map, Either, Reference lens
     
