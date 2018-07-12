85fbf42842e5cadcd15ba20d531e358e45f1081d
NawaMan <nawa@nawaman.net>
2018-07-12 10:11:38 -0600

    Streamable split.

--- functionalj-core/src/main/java/functionalj/types/Streamable.java
+++ functionalj-core/src/main/java/functionalj/types/Streamable.java
@@ -349,6 +349,68 @@ public interface Streamable<DATA, SELF extends Streamable<DATA, SELF>>
         }));
     }
     
+    // TODO - Find a better ways to do it
+    
+    public default Tuple2<SELF, SELF> split(
+            Predicate<? super DATA> predicate) {
+        return ImmutableTuple.of(
+                this.filter(predicate),
+                this.exclude(predicate)
+        );
+    }
+    
+    public default Tuple3<SELF, SELF, SELF> split(
+            Predicate<? super DATA> predicate1,
+            Predicate<? super DATA> predicate2) {
+        return ImmutableTuple.of(
+                this.filter(predicate1),
+                this.exclude(predicate1).filter(predicate2),
+                this.exclude(predicate1).exclude(predicate2)
+        );
+    }
+
+    public default Tuple4<SELF, SELF, SELF, SELF> split(
+            Predicate<? super DATA> predicate1,
+            Predicate<? super DATA> predicate2,
+            Predicate<? super DATA> predicate3) {
+        return ImmutableTuple.of(
+                this.filter(predicate1),
+                this.exclude(predicate1).filter(predicate2),
+                this.exclude(predicate1).exclude(predicate2).filter(predicate3),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3)
+        );
+    }
+
+    public default Tuple5<SELF, SELF, SELF, SELF, SELF> split(
+            Predicate<? super DATA> predicate1,
+            Predicate<? super DATA> predicate2,
+            Predicate<? super DATA> predicate3,
+            Predicate<? super DATA> predicate4) {
+        return ImmutableTuple.of(
+                this.filter(predicate1),
+                this.exclude(predicate1).filter(predicate2),
+                this.exclude(predicate1).exclude(predicate2).filter(predicate3),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3).filter(predicate4),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3).exclude(predicate4)
+        );
+    }
+    
+    public default Tuple6<SELF, SELF, SELF, SELF, SELF, SELF> split(
+            Predicate<? super DATA> predicate1,
+            Predicate<? super DATA> predicate2,
+            Predicate<? super DATA> predicate3,
+            Predicate<? super DATA> predicate4,
+            Predicate<? super DATA> predicate5) {
+        return ImmutableTuple.of(
+                this.filter(predicate1),
+                this.exclude(predicate1).filter(predicate2),
+                this.exclude(predicate1).exclude(predicate2).filter(predicate3),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3).filter(predicate4),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3).exclude(predicate4).filter(predicate5),
+                this.exclude(predicate1).exclude(predicate2).exclude(predicate3).exclude(predicate4).exclude(predicate5)                
+        );
+    }
+    
     public default SELF distinct() {
         return __stream(stream -> stream.distinct());
     }
--- functionalj-core/src/test/java/functionalj/types/StreamableTest.java
+++ functionalj-core/src/test/java/functionalj/types/StreamableTest.java
@@ -1,8 +1,11 @@
 package functionalj.types;
 
+import static functionalj.lens.Access.$S;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import functionalj.lens.Access;
 
 public class StreamableTest {
     
@@ -10,6 +13,14 @@ public class StreamableTest {
     public void testSelectiveMap() {
         assertEquals("[One, --Two, Three, Four, Five]", 
                 ImmutableList.of("One", "Two", "Three", "Four", "Five").selectiveMap("Two"::equals, str -> "--" + str).toString());
+    }
+    @Test
+    public void testSplit() {
+        assertEquals("[[One, Two],[Four, Five],[Three]]", 
+                ImmutableList.of("One", "Two", "Three", "Four", "Five")
+                .split($S.length().thatEquals(3),
+                       $S.length().thatEquals(4))
+                .toString());
     }
     
 }
