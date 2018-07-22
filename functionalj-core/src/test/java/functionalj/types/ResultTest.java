fde9adf09fe4c4f9d2320671291969c6ee709ce1
NawaMan <nawa@nawaman.net>
2018-07-22 14:17:17 -0600

    Organize import.

--- functionalj-core/src/test/java/functionalj/types/ResultTest.java
+++ functionalj-core/src/test/java/functionalj/types/ResultTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 import org.junit.Test;
 
 import functionalj.types.result.Result;
