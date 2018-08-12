a8c4952a806d71aec069e024a8b2ef861df577f0
NawaMan <nawa@nawaman.net>
2018-08-12 04:05:53 -0600

    Organize imports.

--- functionalj-core/src/test/java/functionalj/annotations/uniontype/UnionTypeWithMethodTest.java
+++ functionalj-core/src/test/java/functionalj/annotations/uniontype/UnionTypeWithMethodTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import functionalj.annotations.UnionType;
