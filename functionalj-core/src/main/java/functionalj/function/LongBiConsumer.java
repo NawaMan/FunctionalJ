58082bf80d2d24c1e0edd21f48fc0eb24681e48c
NawaMan <nawa@nawaman.net>
2021-01-05 00:36:53 -0500

    Update license.

--- functionalj-core/src/main/java/functionalj/function/LongBiConsumer.java
+++ functionalj-core/src/main/java/functionalj/function/LongBiConsumer.java
@@ -1,5 +1,5 @@
 // ============================================================================
-// Copyright(c) 2017-2020 Nawapunth Manusitthipol (NawaMan - http://nawaman.net)
+// Copyright(c) 2017-2021 Nawapunth Manusitthipol (NawaMan - http://nawaman.net)
 // ----------------------------------------------------------------------------
 // MIT License
 // 
