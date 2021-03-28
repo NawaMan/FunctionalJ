4c49b4e5b24615f8155bd49a01d4d47c9e05ba69
NawaMan <nawa@nawaman.net>
2021-03-28 15:53:22 -0400

    Add assertAsString.

--- functionalj-core/src/test/java/functionalj/lens/lenses/DoubleToDoubleAccessPrimitiveTest.java
+++ functionalj-core/src/test/java/functionalj/lens/lenses/DoubleToDoubleAccessPrimitiveTest.java
@@ -1,7 +1,7 @@
 package functionalj.lens.lenses;
 
+import static functionalj.TestHelper.assertAsString;
 import static functionalj.lens.Access.theNumber;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -10,17 +10,13 @@ import functionalj.list.intlist.IntFuncList;
 
 public class DoubleToDoubleAccessPrimitiveTest {
     
-    private void assertStrings(String expected, Object actual) {
-        assertEquals(expected, String.valueOf(actual));
-    }
-    
     @Test
     public void testGreaterThan() {
         DoubleFuncList numbers = IntFuncList.range(0, 10).mapToDouble();
-        assertStrings("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(5)));
-        assertStrings("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(()->5)));
-        assertStrings("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(n -> 10 - n)));
-        assertStrings("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(theNumber.minus(10).negate())));
+        assertAsString("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(5)));
+        assertAsString("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(()->5)));
+        assertAsString("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(n -> 10 - n)));
+        assertAsString("[6.0, 7.0, 8.0, 9.0]", numbers.filter(theNumber.thatGreaterThan(theNumber.minus(10).negate())));
     }
     
 }
--- functionalj-core/src/test/java/functionalj/lens/lenses/IntegerToIntegerAccessPrimitiveTest.java
+++ functionalj-core/src/test/java/functionalj/lens/lenses/IntegerToIntegerAccessPrimitiveTest.java
@@ -1,7 +1,7 @@
 package functionalj.lens.lenses;
 
+import static functionalj.TestHelper.assertAsString;
 import static functionalj.lens.Access.theInteger;
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,17 +9,13 @@ import functionalj.list.intlist.IntFuncList;
 
 public class IntegerToIntegerAccessPrimitiveTest {
     
-    private void assertStrings(String expected, Object actual) {
-        assertEquals(expected, String.valueOf(actual));
-    }
-    
     @Test
     public void testGreaterThan() {
         IntFuncList numbers = IntFuncList.range(0, 10);
-        assertStrings("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(5)));
-        assertStrings("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(()->5)));
-        assertStrings("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(n -> 10 - n)));
-        assertStrings("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(theInteger.minus(10).negate())));
+        assertAsString("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(5)));
+        assertAsString("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(()->5)));
+        assertAsString("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(n -> 10 - n)));
+        assertAsString("[6, 7, 8, 9]", numbers.filter(theInteger.thatGreaterThan(theInteger.minus(10).negate())));
     }
     
 }
