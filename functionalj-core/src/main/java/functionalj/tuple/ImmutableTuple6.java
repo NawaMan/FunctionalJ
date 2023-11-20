21008233223d9a206ed1ac4556adcdb76a160cb2
Nawa Manusitthipol <nawa@nawaman.net>
2023-11-20 00:21:12 -0500

    Add withX and JavaDoc to ImmutableTuple6.

--- functionalj-core/src/main/java/functionalj/tuple/ImmutableTuple6.java
+++ functionalj-core/src/main/java/functionalj/tuple/ImmutableTuple6.java
@@ -23,20 +23,52 @@
 // ============================================================================
 package functionalj.tuple;
 
+import functionalj.function.Func0;
+import functionalj.function.Func1;
+
+/**
+ * Represents an immutable 6-element tuple, providing a way to store seven different values of possibly differing types.
+ * This class implements {@link Tuple6}, ensuring that the values cannot be modified after creation.
+ * 
+ * @param <T1>  the type of the first element in the tuple
+ * @param <T2>  the type of the second element in the tuple
+ * @param <T3>  the type of the third element in the tuple
+ * @param <T4>  the type of the fourth element in the tuple
+ * @param <T5>  the type of the fifth element in the tuple
+ * @param <T6>  the type of the sixth element in the tuple
+ */
 public class ImmutableTuple6<T1, T2, T3, T4, T5, T6> implements Tuple6<T1, T2, T3, T4, T5, T6> {
     
+    /** The first element of the tuple. */
     public final T1 _1;
     
+    /** The second element of the tuple. */
     public final T2 _2;
     
+    /** The third element of the tuple. */
     public final T3 _3;
     
+    /** The fourth element of the tuple. */
     public final T4 _4;
     
+    /** The fifth element of the tuple. */
     public final T5 _5;
     
+    /** The sixth element of the tuple. */
     public final T6 _6;
     
+    /**
+     * Constructs an immutable 6-element tuple with the specified values.
+     * Each parameter corresponds to an element in the tuple, with its position in the parameter list 
+     * reflecting its position in the tuple.
+     *
+     * @param _1  the value of the first element
+     * @param _2  the value of the second element
+     * @param _3  the value of the third element
+     * @param _4  the value of the fourth element
+     * @param _5  the value of the fifth element
+     * @param _6  the value of the sixth element
+     */
     public ImmutableTuple6(T1 _1, T2 _2, T3 _3, T4 _4, T5 _5, T6 _6) {
         this._1 = _1;
         this._2 = _2;
@@ -46,29 +78,126 @@ public class ImmutableTuple6<T1, T2, T3, T4, T5, T6> implements Tuple6<T1, T2, T
         this._6 = _6;
     }
     
+    @Override
     public T1 _1() {
         return _1;
     }
     
+    @Override
     public T2 _2() {
         return _2;
     }
     
+    @Override
     public T3 _3() {
         return _3;
     }
     
+    @Override
     public T4 _4() {
         return _4;
     }
     
+    @Override
     public T5 _5() {
         return _5;
     }
     
+    @Override
     public T6 _6() {
         return _6;
     }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with1(T1 new1) {
+        return new ImmutableTuple6<>(new1, _2, _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with1(Func0<T1> supplier1) {
+        return new ImmutableTuple6<>(supplier1.get(), _2, _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with1(Func1<T1, T1> function1) {
+        return new ImmutableTuple6<>(function1.apply(_1()), _2, _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with2(T2 new2) {
+        return new ImmutableTuple6<>(_1, new2, _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with2(Func0<T2> supplier2) {
+        return new ImmutableTuple6<>(_1, supplier2.get(), _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with2(Func1<T2, T2> function2) {
+        return new ImmutableTuple6<>(_1, function2.apply(_2()), _3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with3(T3 new3) {
+        return new ImmutableTuple6<>(_1, _2, new3, _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with3(Func0<T3> supplier3) {
+        return new ImmutableTuple6<>(_1, _2, supplier3.get(), _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with3(Func1<T3, T3> function3) {
+        return new ImmutableTuple6<>(_1, _2, function3.apply(_3()), _4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with4(T4 new4) {
+        return new ImmutableTuple6<>(_1, _2, _3, new4, _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with4(Func0<T4> supplier4) {
+        return new ImmutableTuple6<>(_1, _2, _3, supplier4.get(), _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with4(Func1<T4, T4> function4) {
+        return new ImmutableTuple6<>(_1, _2, _3, function4.apply(_4()), _5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with5(T5 new5) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, new5, _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with5(Func0<T5> supplier5) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, supplier5.get(), _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with5(Func1<T5, T5> function5) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, function5.apply(_5()), _6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with6(T6 new6) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, _5, new6);
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with6(Func0<T6> supplier6) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, _5, supplier6.get());
+    }
+    
+    @Override
+    public Tuple6<T1, T2, T3, T4, T5, T6> with6(Func1<T6, T6> function6) {
+        return new ImmutableTuple6<>(_1, _2, _3, _4, _5, function6.apply(_6()));
+    }
+    
     
     @Override
     public String toString() {
