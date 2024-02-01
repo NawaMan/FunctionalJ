2342eed1073e8959b5483c4e02bcd61282b3e93d
Nawa Manusitthipol <nawa@nawaman.net>
2024-02-01 01:06:15 -0500

    MapThen, MapToTuple for IntFuncList.

--- functionalj-core/src/main/java/functionalj/list/intlist/IntFuncListWithMapThen.java
+++ functionalj-core/src/main/java/functionalj/list/intlist/IntFuncListWithMapThen.java
@@ -24,12 +24,18 @@
 package functionalj.list.intlist;
 
 import static functionalj.list.intlist.IntFuncList.deriveToObj;
+
 import java.util.function.BiFunction;
 import java.util.function.IntFunction;
+
+import functionalj.function.Func10;
 import functionalj.function.Func3;
 import functionalj.function.Func4;
 import functionalj.function.Func5;
 import functionalj.function.Func6;
+import functionalj.function.Func7;
+import functionalj.function.Func8;
+import functionalj.function.Func9;
 import functionalj.list.FuncList;
 
 public interface IntFuncListWithMapThen extends AsIntFuncList {
@@ -68,4 +74,33 @@ public interface IntFuncListWithMapThen extends AsIntFuncList {
     public default <T1, T2, T3, T4, T5, T6, T> FuncList<T> mapThen(IntFunction<? extends T1> mapper1, IntFunction<? extends T2> mapper2, IntFunction<? extends T3> mapper3, IntFunction<? extends T4> mapper4, IntFunction<? extends T5> mapper5, IntFunction<? extends T6> mapper6, Func6<T1, T2, T3, T4, T5, T6, T> function) {
         return deriveToObj(this, stream -> stream.mapThen(mapper1, mapper2, mapper3, mapper4, mapper5, mapper6, function));
     }
+    
+    /**
+     * Map the value into different values and then combine them with the combinator.
+     */
+    public default <T1, T2, T3, T4, T5, T6, T7, T> FuncList<T> mapThen(IntFunction<? extends T1> mapper1, IntFunction<? extends T2> mapper2, IntFunction<? extends T3> mapper3, IntFunction<? extends T4> mapper4, IntFunction<? extends T5> mapper5, IntFunction<? extends T6> mapper6, IntFunction<? extends T7> mapper7, Func7<T1, T2, T3, T4, T5, T6, T7, T> function) {
+        return deriveToObj(this, stream -> stream.mapThen(mapper1, mapper2, mapper3, mapper4, mapper5, mapper6, mapper7, function));
+    }
+    
+    /**
+     * Map the value into different values and then combine them with the combinator.
+     */
+    public default <T1, T2, T3, T4, T5, T6, T7, T8, T> FuncList<T> mapThen(IntFunction<? extends T1> mapper1, IntFunction<? extends T2> mapper2, IntFunction<? extends T3> mapper3, IntFunction<? extends T4> mapper4, IntFunction<? extends T5> mapper5, IntFunction<? extends T6> mapper6, IntFunction<? extends T7> mapper7, IntFunction<? extends T8> mapper8, Func8<T1, T2, T3, T4, T5, T6, T7, T8, T> function) {
+        return deriveToObj(this, stream -> stream.mapThen(mapper1, mapper2, mapper3, mapper4, mapper5, mapper6, mapper7, mapper8, function));
+    }
+    
+    /**
+     * Map the value into different values and then combine them with the combinator.
+     */
+    public default <T1, T2, T3, T4, T5, T6, T7, T8, T9, T> FuncList<T> mapThen(IntFunction<? extends T1> mapper1, IntFunction<? extends T2> mapper2, IntFunction<? extends T3> mapper3, IntFunction<? extends T4> mapper4, IntFunction<? extends T5> mapper5, IntFunction<? extends T6> mapper6, IntFunction<? extends T7> mapper7, IntFunction<? extends T8> mapper8, IntFunction<? extends T9> mapper9, Func9<T1, T2, T3, T4, T5, T6, T7, T8, T9, T> function) {
+        return deriveToObj(this, stream -> stream.mapThen(mapper1, mapper2, mapper3, mapper4, mapper5, mapper6, mapper7, mapper8, mapper9, function));
+    }
+    
+    /**
+     * Map the value into different values and then combine them with the combinator.
+     */
+    public default <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> FuncList<T> mapThen(IntFunction<? extends T1> mapper1, IntFunction<? extends T2> mapper2, IntFunction<? extends T3> mapper3, IntFunction<? extends T4> mapper4, IntFunction<? extends T5> mapper5, IntFunction<? extends T6> mapper6, IntFunction<? extends T7> mapper7, IntFunction<? extends T8> mapper8, IntFunction<? extends T9> mapper9, IntFunction<? extends T10> mapper10, Func10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> function) {
+        return deriveToObj(this, stream -> stream.mapThen(mapper1, mapper2, mapper3, mapper4, mapper5, mapper6, mapper7, mapper8, mapper9, mapper10, function));
+    }
+    
 }
