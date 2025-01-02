332344b674b0954e9d79613fa451b7ea7275f8b8
Nawa Manusitthipol <nawa@nawaman.net>
2025-01-02 01:24:50 -0500

    WIP on Issue38-IndexedItem

--- functionalj-core/src/test/java/functionalj/lens/StringAccessTest.java
+++ functionalj-core/src/test/java/functionalj/lens/StringAccessTest.java
@@ -1,11 +1,13 @@
 package functionalj.lens;
 
+import static functionalj.TestHelper.assertAsString;
 import static functionalj.lens.Access.$S;
 import static functionalj.lens.Access.theString;
 import static functionalj.list.ImmutableFuncList.listOf;
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import functionalj.TestHelper;
+
 import lombok.val;
 
 public class StringAccessTest {
@@ -19,13 +21,28 @@ public class StringAccessTest {
     @Test
     public void testFormatWith() {
         val lists = listOf("ONE", "TWO", "THREE", "AE", "BEE", "SEE");
-        TestHelper.assertAsString("[ONE (3), TWO (3), THREE (5), AE (2), BEE (3), SEE (3)]", lists.map(theString.formatWith("%s (%s)", $S.length())));
+        assertAsString(
+                "[ONE (3), TWO (3), THREE (5), AE (2), BEE (3), SEE (3)]", 
+                lists.map(theString.formatWith("%s (%s)", $S.length())));
+        assertAsString(
+                "[3 (ONE), 3 (TWO), 5 (THREE), 2 (AE), 3 (BEE), 3 (SEE)]", 
+                lists.map(theString.formatWith("%2$s (%1$s)", $S.length())));
+    }
+    
+    @Test
+    public void testConcat() {
+        val lists = listOf("ONE", "TWO", "THREE", "AE", "BEE", "SEE");
+        assertAsString(
+                "[ONE-3, TWO-3, THREE-5, AE-2, BEE-3, SEE-3]", 
+                lists.map(theString.concat("-", $S.length())));
     }
     
     @Test
     public void testGrab() {
         val str = "1, 2,3";
-        assertEquals("[1, 2, 3]", theString.grab("[0-9]+").apply(str).toString());
+        assertEquals(
+                "[1, 2, 3]", 
+                theString.grab("[0-9]+").apply(str).toString());
     }
     
     @Test
